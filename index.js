18d47e3f9077070c1a0ff5b170fc49c503c12011
sanketwakhare <sanketwakhare@gmail.com>
2023-02-07 00:08:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { io } from 'socket.io-client';
+
+// const socket = io('http://localhost:3000');
+
+
+// for (let i = 0; i < 10; i++) {
+//     socket.on("connect", () => {
+//         console.log(socket.id);
+//         socket.send('status?user:' + i, (res) => {
+//             console.log(res);
+//         });
+//     });
+// }
+
+// console.log('client started');
+
